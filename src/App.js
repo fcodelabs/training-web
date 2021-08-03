b5fb638cff3b9ce8afe726ccd37bc1b8b6d95c83
Salitha <salitha@fcodelabs.com>
2021-08-03 16:02:03 +0530

    Created DiaryCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Card, Col, Container, Row} from "react-bootstrap";
+import DiaryCard from "./components/DiaryCard";
+import CardGrid from "./components/CardGrid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container fluid className={'bg-gradient bg-info vh-100'}>
+                    <CardGrid/>
+        </Container>
+    );
 }
 
 export default App;

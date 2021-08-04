b9f17983218a56dba63d789debcdbab8a81fd85b
Kalana97 <rmkalana97@gmail.com>
2021-08-04 11:24:18 +0530

    Created DiaryCardModified

--- src/components/Diarycard.js
+++ src/components/Diarycard.js
@@ -3,30 +3,27 @@ import './Diarycard.css'
 import { useState } from 'react';
 
 function Card({ title, subTitle, body, color }) {
-  const [readMore, setReadMore] = useState(false);
-  var content = body;
-  const extraContent = <div>
-    <p className="extra-content">
-      {content.substring(100, content.length)}
-    </p>
-  </div>
-  const linkName = readMore ? 'Read Less << ' : 'Read More'
-  return (
-    <div className='card-container' style={{ backgroundColor: color }}>
-      <div className='card-title'>
-        <h3>{title}</h3>
-      </div>
-      <div className='card-subtitle'>
-        <h5>{subTitle}</h5>
-      </div>
-      <div className='card-body'>
-        <p>{content.substring(0, 100)}{readMore && extraContent}<a className="read-more-link" onClick={() => { setReadMore(!readMore) }}><h2>{linkName}</h2></a></p>
+    const [readMore, setReadMore] = useState(false);
+    // var content = body;
+    const extraContent = < div >
+        <p className = "extra-content" > { body.substring(100, body.length) } </p> </div>
+    
+    return ( <div className = 'card-container' style = {
+            { backgroundColor: color } } >
+        <div className = 'card-title' >
+        <h3 > { title } </h3> </div> 
+        <div className = 'card-subtitle' >
+        <h5 > { subTitle } </h5> 
+        </div> 
+        <div className = 'card-body' >
+        <p > { body.substring(0, 100) } { readMore && extraContent } < a className = "read-more-link"
+        onClick = {
+            () => { setReadMore(!readMore) } } > < h2 > { readMore ? 'Read Less << ' : 'Read More' } </h2></a > </p>
 
-        {/* {readMore && extraContent} */}
-      </div>
+        { /* {readMore && extraContent} */ } </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Card

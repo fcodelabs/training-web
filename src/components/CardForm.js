a935c6d48f2c708c819c86040ab4f3a2c046e525
Kalana97 <rmkalana97@gmail.com>
2021-08-05 19:29:19 +0530

    Created adding form Modified

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,58 +1,72 @@
-import React from 'react'
-import './CardForm.css'
+import React from "react";
+import "./CardForm.css";
+import DiaryCard from "./Diarycard";
 
-export function CardForm({onCancel, card }) {
-  const id = card && card.id ? card.id : undefined
+export function CardForm() {
+  // const id = card && card.id ? card.id : undefined
 
-  const [term, setTerm] = React.useState(id ? card.term : '')
-  const [definition, setDef] = React.useState(id ? card.definition : '')
+  // const [cards, setCards] = React.useState([]);
+  const [title, setTitle] = React.useState("");
+  const [definition, setDefinition] = React.useState("");
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     // event.preventDefault()
-    alert("Title: " +term + " Description: " +definition)
-    console.log(term)
+    // alert("Title: " +title + " Description: " +definition)
+    // console.log(cards);
+    // setCards([...cards, { title: title, definition: definition }]);
+    clearForm()
+    console.log(title)
     console.log(definition)
-  }
+  };
 
   function clearForm() {
-    setTerm('')
-    setDef('')
-    onCancel && typeof onCancel === 'function' && onCancel()
-  }
-
-  function handleTermChange(event) {
-    const { value } = event.target
-    setTerm(value)
-  }
-
-  function handleDefChange(event) {
-    const { value } = event.target
-    setDef(value)
+    setTitle("");
+    setDefinition("");
   }
   return (
-    <div className="formcard-container">
-      {/* <h4>{id ? 'Update Card' : 'Add Card'}</h4> */}
-      <form onReset={clearForm} onSubmit={handleSubmit}>
-        <div>
-          {/* <label htmlFor="card_term">term</label> */}
-          <textarea id="card_term" value={term} onChange={handleTermChange} placeholder="Submit New"/>
-        </div>
-        <div>
-          {/* <label htmlFor="card_definition">definition</label> */}
-          <textarea
-            id="card_definition"
-            value={definition}
-            onChange={handleDefChange}
-            placeholder= "Enter Description"
-            rows= {5}
+    <div>
+      <div className="formcard-container">
+        {/* <h4>{id ? 'Update Card' : 'Add Card'}</h4> */}
+        <form onSubmit={handleSubmit} >
+          <div>
+            {/* <label htmlFor="card_term">term</label> */}
+            <textarea
+              id="card_term"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+              placeholder="Submit New"
+            />
+          </div>
+          <div>
+            {/* <label htmlFor="card_definition">definition</label> */}
+            <textarea
+              id="card_definition"
+              value={definition}
+              onChange={(event) => setDefinition(event.target.value)}
+              placeholder="Enter Description"
+              rows={5}
+            />
+          </div>
+          <div className="buttons">
+            <button
+              className="primary"
+              type="submit"
+              // onClick={() => handleSubmit()}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        {/* {[...cards].map((card) => (
+          <DiaryCard
+            title={card.title}
+            subTitle="card Subtitle"
+            body="A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovelyIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a doc"
+            color="royalblue"
           />
-        </div>
-        <div className="buttons">
-          <button className="primary" type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
+        ))} */}
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
--- src/components/Diarycard.js
+++ src/components/Diarycard.js
@@ -1,29 +1,43 @@
-import React from 'react';
-import './Diarycard.css'
-import { useState } from 'react';
+import React from "react";
+import "./Diarycard.css";
+import { useState } from "react";
 
 function Card({ title, subTitle, body, color }) {
-    const [readMore, setReadMore] = useState(false);
-    // var content = body;
-    const extraContent = < div >
-        <p className = "extra-content" > { body.substring(100, body.length) } </p> </div>
-    
-    return ( <div className = 'card-container' style = {
-            { backgroundColor: color } } >
-        <div className = 'card-title' >
-        <h3 > { title } </h3> </div> 
-        <div className = 'card-subtitle' >
-        <h5 > { subTitle } </h5> 
-        </div> 
-        <div className = 'card-body' >
-        <p > { body.substring(0, 100) } { readMore && extraContent } < a className = "read-more-link"
-        onClick = {
-            () => { setReadMore(!readMore) } } > < h2 > { readMore ? 'Read Less << ' : 'Read More' } </h2></a > </p>
+  const [readMore, setReadMore] = useState(false);
 
-        { /* {readMore && extraContent} */ } </div>
+  // var content = body;
+  const extraContent = (
+    <div>
+      <p className="extra-content"> {body.substring(100, body.length)} </p>{" "}
+    </div>
+  );
 
-        </div>
-    )
+  return (
+    <div className="card-container" style={{ backgroundColor: color }}>
+      <div className="card-title">
+        <h3> {title} </h3>{" "}
+      </div>
+      <div className="card-subtitle">
+        <h5> {subTitle} </h5>
+      </div>
+      <div className="card-body">
+        <p>
+          {" "}
+          {body.substring(0, 100)} {readMore && extraContent}{" "}
+          <a
+            className="read-more-link"
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {" "}
+            <h2> {readMore ? "Read Less << " : "Read More"} </h2>
+          </a>{" "}
+        </p>
+        {/* {readMore && extraContent} */}{" "}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;

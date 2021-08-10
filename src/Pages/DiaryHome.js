64e1184dbd1eb8389ed00ebfe81cb12e117e2dfb
Salitha <salitha@fcodelabs.com>
2021-08-10 10:23:29 +0530

    merge from ITP-8

--- src/Pages/DiaryHome.js
+++ src/Pages/DiaryHome.js
@@ -1,7 +1,7 @@
-import {useState} from "react";
-import {Col, Container, Row} from "react-bootstrap";
-import NewCardForm from "../Components/NewCardForm";
-import CardGrid from "../Components/CardGrid";
+import React, { useEffect } from 'react'
+import { CardGroup, Col, Row } from 'react-bootstrap'
+import NewCardForm from '../components/NewCardForm'
+import DiaryCard from '../components/DiaryCard'
 
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -34,4 +34,4 @@ function DiaryHome () {
   )
 }
 
-export default DiaryHome;
+export default DiaryHome

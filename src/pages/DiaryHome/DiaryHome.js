2ab2397d5d584d595c0289bcc056872c7d142a50
USER <udaraj08@gmail.com>
2021-08-18 20:16:51 +0530

    Created adding form

--- src/pages/DiaryHome/DiaryHome.js
+++ src/pages/DiaryHome/DiaryHome.js
@@ -1,30 +1,23 @@
 //imporing modules
-import React from 'react'
-import DiaryCard from '../../components/DiaryCard'
-import {
-    Typography,
-    Button,
-    AppBar,
-    Toolbar,
-    Dialog,
-    DialogTitle,
-    DialogContent,
-    DialogActions,
-} from '@material-ui/core';
-
+//importing hooks
+import React, {useState} from 'react'
 //hook for style material components
-import { makeStyles } from '@material-ui/core';
-
-
+import {
+    AppBar,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    makeStyles,
+    TextField,
+    Toolbar
+} from '@material-ui/core';
 //import css files
 import '../../normalize.css'
 import './diaryPage.css'
-
 //Masonary css for grid
-import Masonry from 'react-masonry-css'
-
-//importing hooks
-import { useState } from 'react';
 
 const styles = makeStyles({
     btn: {
@@ -34,12 +27,25 @@ const styles = makeStyles({
             background: "white",
             color: "#34495e"
         },
+    },
+    text: {
+        borderRadius: "10px",
+        borderWidth: "2px",
+        borderColor: '#3498db !important',
+    },
+    areaText: {
+        borderRadius: "10px",
+        borderWidth: "2px",
+        borderColor: '#3498db !important',
+    },
+    hidden: {
+        color: '#3498db'
     }
 })
 
 function DiaryHome() {
 
-    const classess = styles()
+    const clasess = styles()
 
     const breakpoints = {
         default: 3,
@@ -48,7 +54,11 @@ function DiaryHome() {
         500: 1
     }
 
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [formData, setData] = useState({
+        title: "",
+        description: ""
+    });
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -57,113 +67,163 @@ function DiaryHome() {
         setOpen(false);
     };
 
+    function submitForm() {
+
+        console.log(`
+            title: ${formData.title}\n
+            description: ${formData.description}
+        `)
+        handleClose()
+    }
+
+
     return (
         <div className="diary-container fld-clm">
-            {/* <AppBar className="appbar" position="static">
+            <AppBar className="appbar" position="static">
                 <Toolbar>
-
                 </Toolbar>
             </AppBar>
             <div className="data-form center d-flex">
                 <Button
-                    className={classess.btn}
+                    className={clasess.btn}
                     variant="outlined" color="primary" onClick={handleClickOpen}>
                     Add Note
                 </Button>
-                <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-                    <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                <Dialog
+                    fullWidth={true}
+                    onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+                    <DialogTitle
+                        style={{color: "#ED4C67"}}
+                        id="customized-dialog-title" onClose={handleClose}>
                         Add Note
                     </DialogTitle>
                     <DialogContent dividers>
-                        <Typography gutterBottom>
-                            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-                            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-                        </Typography>
-                        <Typography gutterBottom>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-                            lacus vel augue laoreet rutrum faucibus dolor auctor.
-                        </Typography>
-                        <Typography gutterBottom>
-                            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-                            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-                            auctor fringilla.
-                        </Typography>
+                        <FormControl fullWidth={true}>
+                            <TextField
+                                onChange={(e) => {
+                                    setData(prevState => ({
+                                        ...prevState,
+                                        title: e.target.value
+                                    }))
+                                }}
+                                required={true}
+                                // onChange={}
+                                InputProps={{
+                                    classes: {
+                                        notchedOutline: clasess.text,
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    classes: {
+                                        root: clasess.hidden
+                                    }
+                                }}
+                                style={{width: "100%"}}
+                                fullWidth={true}
+                                id="outlined-basic" label="Outlined" variant="outlined"/>
+                            <br/>
+                            <TextField
+                                onChange={(e) => {
+                                    setData(prevState => ({
+                                        ...prevState,
+                                        description: e.target.value
+                                    }))
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: clasess.areaText,
+                                        notchedOutline: clasess.areaText
+                                    }
+                                }}
+                                InputLabelProps={{
+                                    classes: {
+                                        root: clasess.hidden
+                                    }
+                                }}
+                                style={{width: "100%"}}
+                                fullWidth={true}
+                                multiline={4}
+                                rows={10}
+                                id="outlined-basic" label="Outlined" variant="outlined"/>
+                        </FormControl>
                     </DialogContent>
                     <DialogActions>
-                        <Button autoFocus onClick={handleClose} color="primary">
-                            Save changes
+                        <Button autoFocus onClick={() => {
+                            submitForm()
+                        }} variant={"contained"} color="secondary">
+                            SUBMIT
                         </Button>
                     </DialogActions>
                 </Dialog>
             </div>
-            <div className="data-grid">
-                <Masonry
-                    breakpointCols={breakpoints}
-                    className="my-masonry-grid"
-                    columnClassName="my-masonry-grid_column"
-                >
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss
-                    descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                    <div className="diary-card">
-                        <DiaryCard
-                            title="Title"
-                            subtitle="subtitle"
-                            description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss" />
-                    </div>
-                </Masonry>
-            </div>
-            <footer class="footer d-flex">
-                <Typography style={{ paddingBottom: 5 }} gutterBottom variant="subtitle1">
-                    &copy; 2021 Fcode labs | All the copyrights reserved
-                </Typography>
-            </footer> */}
+            {/*<div className="data-grid">*/}
+            {/*    <Masonry*/}
+            {/*        breakpointCols={breakpoints}*/}
+            {/*        className="my-masonry-grid"*/}
+            {/*        columnClassName="my-masonry-grid_column"*/}
+            {/*    >*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss*/}
+            {/*        descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*        <div className="diary-card">*/}
+            {/*            <DiaryCard*/}
+            {/*                title="Title"*/}
+            {/*                subtitle="subtitle"*/}
+            {/*                description="descriptionss descriptionss descriptionss descriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionssdescriptionss"/>*/}
+            {/*        </div>*/}
+            {/*    </Masonry>*/}
+            {/*</div>*/}
+            {/*<footer class="footer d-flex">*/}
+            {/*    <Typography style={{paddingBottom: 5}} gutterBottom variant="subtitle1">*/}
+            {/*        &copy; 2021 Fcode labs | All the copyrights reserved*/}
+            {/*    </Typography>*/}
+            {/*</footer>*/}
         </div>
     )
 }

a59081a2883173708a90038ad33066634fc4afc8
Gathsara <66924429+GathsaraH@users.noreply.github.com>
2021-08-21 18:45:58 +0530

    Declare a function Instead of inline code

--- src/components/DiaryCard/AddNewDiary.js
+++ src/components/DiaryCard/AddNewDiary.js
@@ -2,33 +2,28 @@ import React, {useState} from 'react';
 import classes from './AddNewDiary.module.css'
 import Card from "@material-ui/core/Card";
 
-
-
 function AddNewDiary() {
 
 
     //states
-    const [diaryData, setDiaryData] = useState({
-        title: '',
-        description: ''
-    })
+    const [title,setTitle]=useState('');
+    const [description,setDescription]=useState('')
 
 
     const allData=[{
-        title:diaryData.title,
-        description:diaryData.description
+        title:title,
+        description:description
     }]
 
 
     function submitDataHandler() {
-        if (diaryData.title===''){
+        if (title===''){
             console.log("Title Missing")
-        }else if(diaryData.description===''){
+        }else if(description===''){
             console.log('Description Missing')
         }else {
             console.log(allData)
         }
-
     }
 
     function preventHandler(event) {
@@ -36,23 +31,27 @@ function AddNewDiary() {
 
     }
 
+    //functions for get  values
+    function titleValueHandler(event) {
+        setTitle(event.target.value)
+    }
+
+    function descriptionValueHandler(event) {
+        setDescription(event.target.value)
+    }
+
 
     return (
         <div>
-
             <Card className={classes.card}>
                 <form className={classes.form} onSubmit={preventHandler}>
                     <div className={classes.control}>
-                        <label htmlFor='title'>
-                            Topic
-                        </label>
-                        <input placeholder='Topic' type='text' value={diaryData.title} required id='title' onChange={(e) => {setDiaryData(prevState => ({...prevState, title: e.target.value}))}}/>
+                        <label htmlFor='title'> Topic </label>
+                        <input placeholder='Topic' type='text' value={title} required id='title' onChange={titleValueHandler}/>
                     </div>
                     <div className={classes.control}>
-                        <label htmlFor='description'>
-                            Description
-                        </label>
-                        <textarea placeholder='Description' id='description' value={diaryData.description} required rows='5' onChange={(e) => {setDiaryData(prevState => ({...prevState, description: e.target.value}))}}>
+                        <label htmlFor='description'> Description </label>
+                        <textarea placeholder='Description' id='description' value={description} required rows='5' onChange={descriptionValueHandler}>
                     </textarea>
                         <div className={classes.actions}>
                             <button onClick={submitDataHandler}>Add Your Diary</button>
@@ -64,6 +63,4 @@ function AddNewDiary() {
     );
 }
 
-
 export default AddNewDiary;
-

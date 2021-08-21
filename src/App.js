4ac4a6c1162f79382a14376b2672b1bcc5676202
Binith_105545 <Binith.Jayasinghe@axiatadigitallabs.com>
2021-08-22 04:18:25 +0530

    Created adding form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import DiaryCard from './components/diary-card/diary-card';
+// import DiaryCard from './components/diary-card/diary-card';
 import './App.css';
+import AddForm from './components/add-form/add-form';
+import { useState } from 'react';
 
 function App() {
+
+  const [state, setState] = useState([]);
+  const [input, setInput] = useState({
+    title: '',
+    description: ''
+  });
+
+  // let input = {
+  //   title: '',
+  //   description: ''
+  // }
+
+  const handleChange = (event) => {
+    setInput(({
+      ...input, [event.target.name]: event.target.value
+    }));
+  };
+
+  const handleSubmit = () => {
+
+    console.log("Inputs")
+    console.log(input)
+    setState([...state, input]);
+    
+    setInput({
+      title: '',
+      description: ''
+    })
+  };
+
   return (
     <div className="App">
-      <DiaryCard
+      <AddForm
+        input={input}
+        handleChange={(event) => handleChange(event)}
+        handleSubmit={() => handleSubmit()} />
+
+      {/* <DiaryCard
         title="Resons Why Learning English Is So Important"
         name="Name"
-        description="They should be easy to scan for relevant and actionable information. Elements, like text and images, should be placed on them in a way that clearly indicates hierarchy." />
+        description="They should be easy to scan for relevant and actionable information. Elements, like text and images, should be placed on them in a way that clearly indicates hierarchy." /> */}
     </div>
   );
 }
---
+++ src/components/add-form/add-form.js
@@ -0,0 +1,64 @@
+import { Button, makeStyles, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '90%',
+        }
+    },
+    card: {
+        backgroundColor: "#b3e9fe"
+    },
+    title: {
+        backgroundColor: '#2e94de'
+    },
+    description: {
+        backgroundColor: '#149fdd'
+    },
+    button: {
+        backgroundColor: '#0074eb',
+        width: '90%'
+    }
+}));
+
+function AddForm(props) {
+
+    const classes = useStyles();
+
+    const handleChange = (e) => {
+        props.handleChange(e);
+    }
+
+    return (
+        <div className={classes.card}>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField
+                    className={classes.title}
+                    id="outlined-basic"
+                    value={props.input.title}
+                    onChange={handleChange}
+                    name="title"
+                    label="Outlined"
+                    variant="outlined" />
+                <TextField
+                    className={classes.description}
+                    id="outlined-multiline-static"
+                    value={props.input.description}
+                    onChange={handleChange}
+                    name="description"
+                    label="Multiline"
+                    multiline
+                    rows={5}
+                    variant="outlined" />
+            </form>
+            <Button className={classes.button}
+                onClick={() => props.handleSubmit()}
+                >
+                SHOW MORE
+            </Button>
+        </div>
+    )
+}
+
+export default AddForm

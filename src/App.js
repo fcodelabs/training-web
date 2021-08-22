57e7dfaa6e5cf7b4fd22144d66bf666f55a1ab75
Lochana Thiwanka <lochanathiwanka62@gmail.com>
2021-08-22 21:46:01 +0530

    Implement adding card 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, {useState} from "react";
-import DiaryHome from "./components/DiaryHome";
+import React from "react";
 import './App.css';
-import AppMenuBar from "./components/layouts/AppMenuBar";
-import DiaryCardContainer from "./components/DiaryCardContainer";
-
-/*Context*/
-export const Context = React.createContext('');
-/*Temp Array*/
-let tempData = [];
+import Home from "./pages/Home";
 
 function App() {
-    const [data, setData] = useState();
-
     return (
         <div className="App">
-            <Context.Provider value={[data, setData, tempData]}>
-                <AppMenuBar/>
-                <DiaryHome/>
-                <DiaryCardContainer/>
-            </Context.Provider>
+            <Home/>
         </div>
     );
 }
--- src/components/DiaryCardContainer.js
+++ src/components/DiaryCardContainer.js
@@ -1,36 +1,18 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import DiaryCard from "./DiaryCard";
-import {Context} from "../App";
 import Container from '@material-ui/core/Container';
-import Masonry from 'react-masonry-css'
 
-function DiaryCardContainer() {
-    //context
-    const context = useContext(Context);
-
-    //breakpoints for masonry
-    const breakPoints = {
-        default: 3,
-        1100: 2,
-        700: 1
-    }
-
+function DiaryCardContainer({tempData}) {
     return (
         <Container>
-            <Masonry
-                breakpointCols={breakPoints}
-                className="my-masonry-grid"
-                columnClassName="my-masonry-grid_column"
-            >
-                { context[0] !== undefined && context[2].map((data) => {
-                    return <DiaryCard
-                        key={data.title}
-                        title={data.title}
-                        subtitle='Noah'
-                        description={data.description}
-                    />
-                }) }
-            </Masonry>
+            { tempData.map((data) => {
+                return <DiaryCard
+                    key={data.title}
+                    title={data.title}
+                    subtitle='Noah'
+                    description={data.description}
+                />
+            }) }
         </Container>
     );
 }

27827ffd3f58220ce94d36af865fca08fe27cf14
shamal iroshan <shamaliroshan34@gmail.com>
2021-10-05 19:10:47 +0530

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Posts() {
+    return(
+        <>
+            <h1>This is Cards page page</h1>
+            <duv>
+                this is a card
+            </duv>
+        </>
+    )
+}

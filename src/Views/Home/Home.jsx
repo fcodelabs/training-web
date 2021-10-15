2296b864f514f3cc5750dfce812d386de4129534
KavinduDhananjaya <dhananjayaa1998@gmail.com>
2021-10-15 13:09:56 +0530

    change home tab

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -4,7 +4,11 @@ export default function Home() {
     return(
         <>
             <h1>This is home</h1>
-            <h2>Title of the current tab</h2>
+            <h2>This is the title of the current tab</h2>
+            <p>In Angular, a module is a mechanism to group components, directives, pipes and services that are related,
+                in such a way that can be combined with other modules to create an application.
+                An Angular application can be thought of as a
+                puzzle where each piece (or each module) is needed to be able to see the full picture.</p>
         </>
     )
 }

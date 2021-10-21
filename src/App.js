e960d0cf0aa067bb9bf860636a44d6de3e7794a2
Dulajdeshan <dulajdeshans@gmail.com>
2021-10-21 19:52:11 +0530

    Change app body

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div>
+           <p>Testing Application for Training People</p>
+       </div>
         <a
           className="App-link"
           href="https://reactjs.org"

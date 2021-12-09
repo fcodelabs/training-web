3e71d0d1ec2b4e8e570ff4a507533afccd3c492a
udith-kavinda <udithkavinda650@gmail.com>
2021-12-09 08:49:35 +0530

    Created diary card

---
+++ src/components/DiaryCard/DiaryCard.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import Button from '@mui/material/Button'
+import './DiaryCard.css'
+
+export const DiaryCard = ({props}) => {
+
+    const [buttonClicked, setButtonClicked] = useState(false)
+    let title = props.title
+    let name = props.name
+    let description = props.description
+    let desc = ''
+    let backgroundColor = props.name
+
+    if(!buttonClicked && description.length < 100){
+        desc = description
+    }
+    if(!buttonClicked && description.length > 100){
+        desc = description.substr(0,100) + "..."
+    }
+    if(buttonClicked){
+        desc = description
+    }
+
+    return (
+        <div className="card lg" style={{background: backgroundColor}}>
+            <div className="cardContent">
+                <div className="headers">
+                    <div className="title">
+                        <h2 className="cardTitle">{title}</h2>
+                        <p className="subTitle text">{name}</p>
+                        <p className="description text">{desc}</p>
+                    </div>
+                    <div className="btnClass">
+                        <Button
+                            style={{borderRadius: '15px', fontWeight:'600', color:'#171d1f'}}
+                            onClick = {() => setButtonClicked(!buttonClicked)} 
+                            className="showMore">{!buttonClicked ? "Show More" : "Show Less"}</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+

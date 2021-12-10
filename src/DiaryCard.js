672a9c46baa09bf0dabd46b31205166efd688a06
anw98 <adeesha.18@cse.mrt.ac.lk>
2021-12-10 08:23:51 +0530

    DiaryCard requested changes updated

--- src/DiaryCard.js
+++ src/DiaryCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import { CardActions } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";

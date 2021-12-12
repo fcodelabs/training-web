7bac5c341a23cf1304db7c747d169f1f798dbfc8
Thushan97 <63712032+Thushan97@users.noreply.github.com>
2021-12-12 17:38:44 +0530

    Implement adding card 2

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ function App() {
       <div className='card-home'>
         <DiaryHome/>
       </div>
-      {/* <div className="App">
-        <DiaryCard title="Reasons Why Learning English is so Important" subtitle="Noah" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam aspernatur eos sequi odit deserunt unde libero officiis nostrum odio, soluta dignissimos, ipsum eius placeat similique ex quae obcaecati laborum ut!" color="#AFEEEE"/>
-        <DiaryCard title="Reasons Why Learning English is so Important" subtitle="Noah" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam aspernatur eos sequi odit deserunt unde libero officiis nostrum odio, soluta dignissimos, ipsum eius placeat similique ex quae obcaecati laborum ut!" color="#AFEEEE"/>
-        <DiaryCard title="Reasons Why Learning English is so Important" subtitle="Noah" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam aspernatur eos sequi odit deserunt unde libero officiis nostrum odio, soluta dignissimos, ipsum eius placeat similique ex quae obcaecati laborum ut!" color="#AFEEEE"/>
-        <DiaryCard title="Reasons Why Learning English is so Important" subtitle="Noah" description="Lorem, ipsum dolor" color="#AFEEEE"/>
-      </div> */}
+      
     </>
   );
 }
--- src/DiaryCard/DiaryCard.jsx
+++ src/DiaryCard/DiaryCard.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './DiaryCard.css';
 import '../App.css';
+import { useState } from 'react';
 
 export default function DiaryCard(props) {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(true);
+    }
 
     return (
         <div className="card" style={{backgroundColor: props.color}} align="left">
             <div className="cardBody">
                 <h2 className="cardTitle">{props.title}</h2>
                 <h4 className="subTitle">{props.subtitle}</h4>
-                {props.description.length <= 100 ? (<p className="cardDescription">{props.description}</p>) : 
-                (<p className="cardDescription">{props.description.substring(0,100)+"..."}</p>)}
-                <button className="cardButton">SHOW MORE</button>
+                {clicked ? <p className="cardDescription">{props.description}</p>
+                    : (props.description.length <= 100 ? (<p className="cardDescription">{props.description}</p>) : 
+                    <p className="cardDescription">{props.description.substring(0,100)+"..."}</p>)}
+                <button className="cardButton" onClick={handleClick} >SHOW MORE</button>
             </div>    
         </div>
     )

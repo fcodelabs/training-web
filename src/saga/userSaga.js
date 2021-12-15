d56c1e0db6e105c2663492b3b203616a02d2ca68
anw98 <adeesha.18@cse.mrt.ac.lk>
2021-12-15 12:58:00 +0530

    changed

--- src/saga/userSaga.js
+++ src/saga/userSaga.js
@@ -2,12 +2,9 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 
 const messagesRef = firestore.collection("Cards").doc("document");
 
-export function* addCard() {
-    console.log("saga");
-    yield takeEvery("FETCHED_List", fetchListAsync);
-  }
+
   
-  export function* fetchListAsync() {
+  export function* addCard() {
     try {
       yield put(requestList());
       const data = yield call(() => {
@@ -24,41 +21,41 @@ export function* addCard() {
 
 //////////////////////////////////
 
-import { takeEvery, put, all, call } from "redux-saga/effects";
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import { firestore } from ".././firebase.utils";
+// import { takeEvery, put, all, call } from "redux-saga/effects";
+// import { createStore, applyMiddleware } from "redux";
+// import createSagaMiddleware from "redux-saga";
+// import { firestore } from ".././firebase.utils";
 
 
-function* rootSaga() {
-  yield all([call(watchFetchList)]);
-}
+// function* rootSaga() {
+//   yield all([call(watchFetchList)]);
+// }
 
-export function* watchFetchList() {
-  console.log("saga");
-  yield takeEvery("FETCHED_List", fetchListAsync);
-}
+// export function* watchFetchList() {
+//   console.log("saga");
+//   yield takeEvery("FETCHED_List", fetchListAsync);
+// }
 
-export function* fetchListAsync() {
-  try {
-    yield put(requestList());
-    const data = yield call(() => {
-      return messagesRef.get().then((doc) => doc.data());
-    });
-    /*     
-      fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      ).then((responde) => responde.json());
-    });*/
-    console.log(data);
-    yield put(requestListSuccess(data));
-  } catch (error) {
-    yield put(requestListError());
-  }
-}
+// export function* fetchListAsync() {
+//   try {
+//     yield put(requestList());
+//     const data = yield call(() => {
+//       return messagesRef.get().then((doc) => doc.data());
+//     });
+//     /*     
+//       fetch(
+//         "https://jsonplaceholder.typicode.com/users"
+//       ).then((responde) => responde.json());
+//     });*/
+//     console.log(data);
+//     yield put(requestListSuccess(data));
+//   } catch (error) {
+//     yield put(requestListError());
+//   }
+// }
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(rootSaga);
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+// sagaMiddleware.run(rootSaga);
 
-export default store;
+// export default store;

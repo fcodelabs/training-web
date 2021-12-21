eade9f43329438f366416c58e8f445244647dd3c
udithkavinda <udith@fcodelabs.com>
2021-12-21 23:39:48 +0530

    sorted card list according to created date & time

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,7 +2,7 @@ export default function reducer (state =[], action) {
   switch (action.type){
     case "addFirebaseData":
       state = action.payload
-      return state
+      return state.sort((a,b) => (a.created < b.created ? 1: -1))
     default:
       return state;
   }

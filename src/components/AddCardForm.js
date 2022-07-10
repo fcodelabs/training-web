1e9af1bbb0487dd568b4524ab59386227b8e42bd
owinog <owingunawardena0123@gmail.com>
2022-07-11 00:50:06 +0530

    added validation

--- src/components/AddCardForm.js
+++ src/components/AddCardForm.js
@@ -1,15 +1,32 @@
-import { Button, TextField, Box, FormGroup } from "@mui/material";
+import { Button, TextField, Box } from "@mui/material";
 import { useState } from "react";
 
 const AddCardForm = () => {
-    const [title, setTitle] = useState();
-    const [description, setDescription] = useState();
+    const [showCollapsed, setShowCollapsed] = useState(false);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
     const handleTitleChange = (event) => {
         setTitle(event.target.value);
     };
     const handleDescriptionChange = (event) => {
         setDescription(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (title === "") {
+            console.log("Enter Title");
+            return;
+        }
+        if (description === "") {
+            console.log("Enter Description");
+            return;
+        }
+
+        setTitle("");
+        setDescription("");
     };
 
     return (
@@ -26,59 +43,55 @@ const AddCardForm = () => {
                 },
             }}
         >
-            <FormGroup>
-                <TextField
-                    sx={{
-                        "& .MuiOutlinedInput-root": {
-                            borderRadius: 10,
-                            backgroundColor: "#2E9FDB",
-                            width: "80%",
-                        },
-                    }}
-                    placeholder="Title"
-                    variant="outlined"
-                    value={title}
-                    onFocus={() => {
-                        console.log("Expand form fields on focus");
-                    }}
-                    onChange={handleTitleChange}
-                />
-                <TextField
-                    sx={{
-                        "& .MuiOutlinedInput-root": {
-                            borderRadius: 3,
-                            backgroundColor: "#2E9FDB",
-                            width: "80%",
-                        },
-                    }}
-                    placeholder="Description"
-                    variant="outlined"
-                    value={description}
-                    onChange={handleDescriptionChange}
-                    multiline
-                    minRows={4}
-                />
-            </FormGroup>
-            <Button
+            <TextField
                 sx={{
-                    ml: 3,
-                    mt: 2,
-                    borderRadius: 5,
-                    pl: 5,
-                    pr: 5,
-                    color: "black",
-                    backgroundColor: "#2E9FDB",
+                    "& .MuiOutlinedInput-root": {
+                        borderRadius: 10,
+                        backgroundColor: "#2E9FDB",
+                        minWidth: "40%",
+                    },
                 }}
-                type="Submit"
+                placeholder="Title"
                 variant="outlined"
-                onClick={(event) => {
-                    event.preventDefault();
-                    console.log("Title -", title);
-                    console.log("Description -", description);
-                }}
-            >
-                Add New
-            </Button>
+                value={title}
+                onClick={() => setShowCollapsed(true)}
+                onChange={handleTitleChange}
+            />
+            {showCollapsed ? (
+                <>
+                    <TextField
+                        sx={{
+                            "& .MuiOutlinedInput-root": {
+                                borderRadius: 3,
+                                backgroundColor: "#2E9FDB",
+                                width: "80%",
+                            },
+                        }}
+                        placeholder="Description"
+                        variant="outlined"
+                        value={description}
+                        onChange={handleDescriptionChange}
+                        multiline
+                        minRows={4}
+                    />
+                    <Button
+                        sx={{
+                            ml: 3,
+                            mt: 2,
+                            borderRadius: 5,
+                            pl: 5,
+                            pr: 5,
+                            color: "black",
+                            backgroundColor: "#2E9FDB",
+                        }}
+                        type="Submit"
+                        variant="outlined"
+                        onClick={handleSubmit}
+                    >
+                        Add New
+                    </Button>
+                </>
+            ) : null}
         </Box>
     );
 };

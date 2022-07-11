8daa81646d85f95f41ce352fbc14d65e00090da3
primesh <primeshheshan@gmail.com>
2022-07-11 22:51:29 +0530

    created rootSaga & added to run function

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import { all } from 'redux-saga/effects';
 import diariesSaga from '../sagas/diariesSaga';
 import diarySlice from './diary-slice';
 
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga() {
+  yield all([...diariesSaga]);
+}
 
 const store = configureStore({
   reducer: {
@@ -14,6 +19,6 @@ const store = configureStore({
 });
 
 //run the saga
-sagaMiddleware.run(diariesSaga);
+sagaMiddleware.run(rootSaga);
 
 export default store;

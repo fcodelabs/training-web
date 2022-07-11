e7adf98ecadccf40cc42e1e386e0fefacdda6cec
primesh <primeshheshan@gmail.com>
2022-07-12 00:09:51 +0530

    add removeDiaried & saveDiaries reducers

--- src/store/diary-slice.js
+++ src/store/diary-slice.js
@@ -8,29 +8,10 @@ const diarySlice = createSlice({
     isChangeStore: false,
   },
   reducers: {
-    addDiary: (state, action) => {
-      const newDiary = action.payload;
-      state.totalDiaries++;
-      state.isChangeStore = true;
-
-      state.diaries.push({
-        id: newDiary.id,
-        name: newDiary.name,
-        title: newDiary.title,
-        description: newDiary.description,
-        cardColor: newDiary.cardColor,
-      });
-    },
-    removeDiary: (state, action) => {
-      const id = action.payload;
-      state.totalDiaries--;
-      state.isChangeStore = true;
-      state.diaries = state.diaries.filter((diary) => diary.id !== id);
-    },
     saveDiaries: (state, action) => {
       state.isChangeStore = true;
-      // state.totalDiaries = action.payload.totalDiaries;
       const newDiaries = action.payload;
+
       newDiaries.map((diary) =>
         state.diaries.push({
           id: diary.id,
@@ -41,11 +22,33 @@ const diarySlice = createSlice({
         })
       );
     },
-    saveTotalDiaries: (state, action) => {
-      const totalDiaries = action.payload;
-      state.totalDiaries = totalDiaries;
-      console.log(action.payload);
+
+    replaceDiary: (state, action) => {
+      const newDiaries = action.payload;
+      state.diaries.length = 0;
+
+      newDiaries.map((diary) =>
+        state.diaries.push({
+          id: diary.id,
+          name: diary.name,
+          title: diary.title,
+          description: diary.description,
+          cardColor: diary.cardColor,
+        })
+      );
     },
+
+    removeDiary: (state, action) => {
+      state.isChangeStore = false;
+      const id = action.payload.id;
+      state.totalDiaries--;
+      state.diaries = state.diaries.filter((diary) => diary.id !== id);
+    },
+
+    saveTotalDiaries: (state, action) => {
+      state.totalDiaries = action.payload;
+    },
+
     getDiaries: () => {},
   },
 });

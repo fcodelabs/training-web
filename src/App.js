78f51316fa1a91a9e15f9ee75b986bef6f3c7ad0
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2022-08-01 17:20:14 +0530

    Added SignIn Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 
+import logo from "../src/images/logo.png";
+//import {genName} from "./signin";
+import {useState} from 'react';
+
 function App() {
+  const [randomName, setRandomName]=useState(null)
+  const genName=()=>{
+    const num=Math.floor(Math.random() * 5)
+        let name
+        let nameArr =['Nimal','Kamal','Peter','Saman','Jhon']
+        setRandomName(nameArr[num])
+  }
+   /*const getRandomVale=()=>
+  {
+   // console.log("ghjk")
+   const [num, setNum] = useState(0);
+   function genNumber(){
+    return Math.floor(Math.random() * 5)
+   }
+    //return genName
+   const click=()=>{
+    setNum(genNumber());
+   }
+  }*/
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +40,24 @@ function App() {
         >
          This is the test app
         </a>
-      </header>
+      </header> */}
+
+      <div className="contain">
+      <div className="image">
+          <img src={logo} width="50px" height="50px"></img>
+        </div>
+        <div className="dh2">
+          <h2>Dear Diary</h2>
+        </div>
+        <div className="table">
+        <h1 id="sh1" text-align="center">Sign In</h1>
+        <input type="text" id="txt1" placeholder="Enter" value={randomName}></input>
+        <input type="Button" id="btn1" value="RANDOM" onClick={genName}></input><br></br>
+        <input type="Button" id="btn2" value="Continue"  disabled={randomName===null}></input>
+        </div>
+      </div>
+
+
     </div>
   );
 }
---
+++ src/signin.js
@@ -0,0 +1,6 @@
+export function genName() 
+{
+    console.log("asdf")
+    
+
+}

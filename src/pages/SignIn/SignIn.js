c53777060c4cf4e7254bc7a9b4aedd1fd265cc5b
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2022-08-03 18:34:56 +0530

    Design SignIn page using material UI

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -3,6 +3,10 @@ import '../../utils/signIn.css';
 
 import logo from "../../asset/images/logo.png";
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 import {useState} from 'react';
 
 function SignIn() {
@@ -25,11 +29,15 @@ function SignIn() {
         <div className="dh2">
           <h2>Dear Diary</h2>
         </div>
-        <div className="table">
+        <div className="signInForm">
         <h1 id="sh1" text-align="center">Sign In</h1>
-        <input type="text" id="txt1" placeholder="Enter" value={randomName}></input>
+        {/* <input type="text" id="txt1" placeholder="Enter" value={randomName}></input>
         <input type="Button" id="btn1" value="RANDOM" onClick={genName}></input><br></br>
-        <input type="Button" id="btn2" value="Continue"  disabled={randomName===null}></input>
+        <input type="Button" id="btn2" value="Continue"  disabled={randomName===null}></input> */}
+        <TextField id="txt1" label="Your NickName" variant="outlined" value={randomName} onChange={event => setRandomName(event.target.value)}/>
+        
+        <Button variant="contained" id="btn1" onClick={genName}>RANDOM</Button>
+        <Button variant="contained" id="btn2" onClick={navigateToDiaryCard} disabled={randomName===null}>Continue</Button>
         </div>
       </div>
 

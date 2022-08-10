c654ff569fec7942edf9a68388b2a0654e32f9ff
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2022-08-10 19:23:53 +0530

    Added firebase

--- src/Redux/ConfigStore.js
+++ src/Redux/ConfigStore.js
@@ -1,10 +1,13 @@
-import {combineReducers } from 'redux';
+//import {combineReducers } from 'redux';
 //import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
 import { configureStore } from '@reduxjs/toolkit';
 //import { Reducer } from 'react';
 //import { Reducer } from '@reduxjs/toolkit';
 import dataReducer from './slices/dataSlice';
 
+import createSagaMiddleware from "redux-saga";
+import dataSaga from './saga/dataSaga';
+const saga = createSagaMiddleware();
 
 
 
@@ -13,8 +16,13 @@ import dataReducer from './slices/dataSlice';
   
 // });
 
-const store = configureStore({reducer : {dataReducer:dataReducer}});
+const store = configureStore({
+    reducer : {
+        dataReducer:dataReducer,
+    },
+    middleware: [saga],
+});
 
-
+saga.run(dataSaga);
 
 export default store;

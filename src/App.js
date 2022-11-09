887cf3c3383982c6b0dd1d0206c0c26e2296708d
abdullanalimm <abdullanalimm@gmail.com>
2022-11-09 10:20:29 +0530

    display issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import SignIn from "./components/SignIn";
+import store from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <SignIn/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <SignIn />
+      </div>
+    </Provider>
   );
 }
 

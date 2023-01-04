69cbdbd48eecf3b131515d6bf2d94cc793aaf681
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-01-04 18:15:04 +0530

    Created DiaryCard

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import SignIn from './pages/SignIn/index';
 import Home from './pages/DairyHome/index';
 import AddingForm from './components/AddingForm/index';
+import DiaryCard from './components/DiaryCard';
 
 
 function App() {
   return (
     <div className="App">
-       <Home />
+       <DiaryCard title='Hi' description='Explanation: As we can see from the above code Child1 component is calling the multiple props using method 1, by creating a separate method and the child2 component is calling the multiple props by creating an anonymous method.' name='Piyumi'  />
     </div>
     
   );

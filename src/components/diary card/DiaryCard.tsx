cfa1fc0202518d7a8adfda0d1071cd185c99e54a
Jalitha99 <jalithakalsara2@gmail.com>
2023-01-05 19:56:35 +0530

    diary card

--- src/components/diary card/DiaryCard.tsx
+++ src/components/diary card/DiaryCard.tsx
@@ -4,6 +4,9 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
+import {Box} from '@mui/material';
+import { useState } from 'react';
 
 interface MyProps {
     title: string;
@@ -13,26 +16,31 @@ interface MyProps {
 const DiaryCard = (props : MyProps) => {
     const title = props.title;
     const description = props.description;
+    const [showMore, setShowMore] = useState(false);
     return ( 
-        <>
-    <Card sx={{ minWidth: 100 , maxWidth:300,
+    
+        <Box width={'300px'}>
+    <Card sx={{ minWidth: 100 , maxWidth:300, display:'flex', flexDirection:'column',
          margin:'1%', bgcolor:'#ADD8E6', borderRadius:'5%', boxShadow:'0'}}>
-      <CardContent>
+
+      <CardContent sx={{display:'flex', flexDirection:'column', maxWidth:300}}>
         <Typography sx={{ fontSize: 24 }} variant="subtitle1" color="black" gutterBottom>
           {title}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           Name
         </Typography>
-        <Typography variant="body2">
-          {description}
+        <Typography variant="body2" gutterBottom={true}>
+          {showMore ? description : description.substring(0, 100)}
         </Typography>
       </CardContent>
       <CardActions>
-      <Button variant="outlined">Show more</Button>
+      <Button variant="outlined" onClick={() => setShowMore(!showMore)}>{!showMore ? "Show More" : "Hide"}</Button>
       </CardActions>
+    
     </Card>
-        </>
+    </Box>
+       
      );
 }
  

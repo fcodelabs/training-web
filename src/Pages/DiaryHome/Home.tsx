b8225d9cc56dd8bdc6c4918548debf1e570a6961
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2023-01-09 13:17:48 +0530

    full-screen function added

--- src/Pages/DiaryHome/Home.tsx
+++ src/Pages/DiaryHome/Home.tsx
@@ -5,29 +5,43 @@ import Cards from "../../components/DiaryCard/Cards";
 import { Container } from "reactstrap";
 import { useDispatch } from "react-redux";
 import { getDiaries } from "../../Pages/DiaryHome/diaryRedux";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 function Home() {
   const dispatch = useDispatch();
+  const handle = useFullScreenHandle();
 
   useEffect(() => {
     dispatch(getDiaries());
   });
 
   return (
-    <Container
-      style={{
-        height: "100%",
-        minHeight: "100vh",
-        backgroundImage: `url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-71554.jpg?w=1060&t=st=1672743749~exp=1672744349~hmac=7abe054ff9ed4063b56bd8090a73e0014095fe7f96078bca1fe2fe4e47b88134)`,
-      }}
-    >
-      <Header />
-      <h1 style={{ fontWeight: "bold", color: "white", marginLeft: "20px" }}>
-        Home
-      </h1>
-      <AddCard />
-      <Cards />
-    </Container>
+    <div>
+      <FullScreen handle={handle}>
+        <Container
+          style={{
+            overflow: "auto",
+            height: "100%",
+            minHeight: "100vh",
+            backgroundImage: `url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-71554.jpg?w=1060&t=st=1672743749~exp=1672744349~hmac=7abe054ff9ed4063b56bd8090a73e0014095fe7f96078bca1fe2fe4e47b88134)`,
+          }}
+        >
+          <Header handle={handle} />
+          <h1
+            style={{
+              fontWeight: "bold",
+              color: "white",
+              marginLeft: "20px",
+              marginTop: "60px",
+            }}
+          >
+            Home
+          </h1>
+          <AddCard />
+          <Cards />
+        </Container>
+      </FullScreen>
+    </div>
   );
 }
 
--- src/components/HomeComponents/Header.tsx
+++ src/components/HomeComponents/Header.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import CropFreeIcon from "@mui/icons-material/CropFree";
+import PersonIcon from "@mui/icons-material/Person";
 
-function Header() {
-  return <div>Header</div>;
+interface Props {
+  handle: any;
+}
+
+function Header(props: Props) {
+  const [fullScreen, setFullScreen] = useState(false);
+
+  const handleClick = (func: any) => {
+    setFullScreen(!fullScreen);
+    if (fullScreen) {
+      func.exit();
+    } else {
+      func.enter();
+    }
+  };
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        left: "0",
+        right: "0",
+        backgroundColor: "#0081C9",
+        zIndex: "1",
+      }}
+    >
+      <button
+        onClick={() => {
+          handleClick(props.handle);
+        }}
+        title={fullScreen ? "Exit Full Screen" : "Enter Full Screen"}
+      >
+        <CropFreeIcon fontSize="large" sx={{ p: 1 }} />
+      </button>
+
+      <button title="Profile" style={{ float: "right" }}>
+        <PersonIcon fontSize="large" sx={{ p: 1 }} />
+      </button>
+    </div>
+  );
   //not implement
 }
 

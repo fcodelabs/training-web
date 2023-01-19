d6cb16b0f4fa12571a1dbc924ca5b6650cdd3219
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2023-01-19 12:14:44 +0530

    .env added

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -8,13 +8,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtqtr8pOA26G0ElhqeJLm9opaY3H_5x8M",
-  authDomain: "dear-diary-piyumi.firebaseapp.com",
-  projectId: "dear-diary-piyumi",
-  storageBucket: "dear-diary-piyumi.appspot.com",
-  messagingSenderId: "519479135871",
-  appId: "1:519479135871:web:a32d0825940d1dac35394a",
-  measurementId: "G-P6F5D4MQ75"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase

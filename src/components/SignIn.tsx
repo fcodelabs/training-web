679b7249980fffae6d538374329bc5f3da7a9b89
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-06 16:16:57 +0530

    something updated

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,18 +1,24 @@
 import { Grid,Paper,Avatar, TextField,Button} from "@mui/material";
 import  React from "react";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Margin } from "@mui/icons-material";
 
 const SignIn = () => {
 
     const paperStyle={
         padding:20,
         height:"70vh",
-        width:280,
+        width:400,
         margin:"20px auto"
     }
 
     const avatarStyle={
-        backgroundColor:"#3e6fc3"
+        backgroundColor:"#3e6fc3",
+        margin: "5px auto"
+    }
+
+    const btnStyle={
+        margin:"12px 20px"
     }
 
     return(
@@ -23,8 +29,8 @@ const SignIn = () => {
                     <h2>Sign In</h2>
                 </Grid>
                
-                <TextField label="user name" placeholder="enter user Name"  fullWidth required />
-                <Button variant="contained" type="submit" color="primary"  fullWidth>Sign In</Button>
+                <TextField label="user name" placeholder="enter user Name" required />
+                <Button variant="contained" type="submit" color="primary" style={btnStyle}>Conform</Button>
                 
             </Paper>
         </Grid>

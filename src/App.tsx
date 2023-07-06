e902f6a276ff308c53f6ee54691300c1072b53f4
exagenio <123676288+exagenio@users.noreply.github.com>
2023-07-06 16:19:07 +0530

    added comments to "Created Sign-in page"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Signin from './containers/Signin/Signin'
+import Signin from './containers/Signin/Signin';
+
+// Create a browser router configuration with routes
 const router = createBrowserRouter([
   { path:'/', element:<Signin/>}
 ])
+// Render the application with the RouterProvider and the configured router
 function App() {
   return (
     <RouterProvider router = {router}/>
--- src/containers/Signin/Signin.tsx
+++ src/containers/Signin/Signin.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { Container, Card, TextField, Typography, Button } from "@mui/material";
 import EastIcon from "@mui/icons-material/East";
 function Signin() {
+    // set variables for nickname and disabled state
   const [nickname, setNickname] = useState("");
   const [isDisabled, setDisabled] = useState(true);
 
+  // Generates a random name from a list
   const randomNameGenerate = () => {
     const names = [
       "John",
@@ -21,24 +23,27 @@ function Signin() {
     return names[randomNumber];
   };
 
+  // Click event handler for the "Random" button
   function clickHandler() {
-    setNickname(randomNameGenerate);
-    setDisabled(false);
+    setNickname(randomNameGenerate);// Set a random name
+    setDisabled(false);// Enable the "Continue" button
   }
+
+  // Change event handler for the nickname input field
   function changeHandler(event: { target: { value: any } }) {
-    console.log("changed");
-    console.log(event.target.value);
-    setNickname(event.target.value);
+    setNickname(event.target.value);// Update the nickname state
     if (event.target.value !== "") {
-      setDisabled(false);
+      setDisabled(false); // If the input field is not empty, enable the "Continue" button
     } else {
-      setDisabled(true);
+      setDisabled(true); // If the input field is empty, disable the "Continue" button
     }
   }
 
+   // Click event handler for the "Continue" button
   function continueHandler(){
-    window.location.href = "/home";
+    window.location.href = "/home"; // Redirect to the home page
   }
+  
   return (
     <React.Fragment>
       <Container

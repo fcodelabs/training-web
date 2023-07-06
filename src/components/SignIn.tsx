deb16ce0c62c4082bfe29972ecad073213a59c50
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-06 16:02:23 +0530

    add sign in body

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,23 @@
+import { Grid,Paper,Avatar, TextField,Button} from "@mui/material";
+import  React from "react";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 const SignIn = () => {
-    
+
+    return(
+        <Grid>
+            <Paper elevation={10}>
+                <Grid>
+                    <Avatar><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+               
+                <TextField label="user name" placeholder="enter user Name"  fullWidth required />
+                <Button variant="contained" type="submit" color="primary"  fullWidth>Sign In</Button>
+                
+            </Paper>
+        </Grid>
+    )
 }
 
 export default SignIn;

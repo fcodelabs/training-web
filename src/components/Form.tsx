540a85a4b7dbc6370543a9f1602203c2b05a14f4
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-07 14:55:02 +0530

    updated conetnt

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,9 @@
 import Box from '@mui/material/Box';
 import {Container,TextField,Button} from '@mui/material';
 import Textarea from '@mui/joy/Textarea';
-//import CssBaseline from '@mui/material/CssBaseline';
 import  React  from "react";
 import { useState } from 'react';
+
 
 const Form = () => {
     const textStyles = {
@@ -16,14 +16,12 @@ const Form = () => {
       }
 
     return(
-        <Container sx={{ bgcolor: '#cfe8fc'}}>
-          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+       
+        <Container sx={{ bgcolor: '#cfe8fc', height: '100vh'}} >
           <TextField InputProps={{style:textStyles}} variant="outlined" placeholder='Submit New' id="fullWidth" />
           <Button sx={{margin:"10px 20px auto",borderRadius:"30px",width:"150px",bgcolor:"#A0BFE0",color:"#ffffff"}} variant="outlined" type='submit'>Submit</Button>
           {/* <Button variant="contained" type="submit" color="primary" sx={{ margin:"20px auto"}}>SIGN IN</Button> */}
-                  
           <Textarea minRows={2} placeholder='Enter Description...' sx={{bgcolor:"#A0BFE0", marginTop:"20px"}} />
-          </Box>
         </Container>
      
       )

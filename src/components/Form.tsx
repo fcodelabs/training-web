55e0fb5b3d7dec0fc7d3d271292760234ae18ff7
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-07 15:10:49 +0530

    print the data in console log

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,6 +6,34 @@ import { useState } from 'react';
 
 
 const Form = () => {
+
+  const [title,setTitle] = useState<string>();
+  const [description,setDescription] = useState<string>();
+
+  const clearFields = () => {
+    setTitle("");
+    setDescription("");
+  }
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+  
+
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+  };
+  
+
+
+  function handleSubmit  (){
+    console.log(title);
+    console.log(description);
+    clearFields();
+    
+  }
+
+
     const textStyles = {
         padding:"5px",
         width:"80vw",
@@ -18,10 +46,10 @@ const Form = () => {
     return(
        
         <Container sx={{ bgcolor: '#cfe8fc', height: '100vh'}} >
-          <TextField InputProps={{style:textStyles}} variant="outlined" placeholder='Submit New' id="fullWidth" />
-          <Button sx={{margin:"10px 20px auto",borderRadius:"30px",width:"150px",bgcolor:"#A0BFE0",color:"#ffffff"}} variant="outlined" type='submit'>Submit</Button>
+          <TextField InputProps={{style:textStyles}} variant="outlined" placeholder='Submit New' id="fullWidth" value={title} onChange={handleTitleChange}  />
+          <Button sx={{margin:"10px 20px auto",borderRadius:"30px",width:"150px",bgcolor:"#A0BFE0",color:"#ffffff"}} variant="outlined" type='submit' onClick={handleSubmit}>Submit</Button>
           {/* <Button variant="contained" type="submit" color="primary" sx={{ margin:"20px auto"}}>SIGN IN</Button> */}
-          <Textarea minRows={2} placeholder='Enter Description...' sx={{bgcolor:"#A0BFE0", marginTop:"20px"}} />
+          <Textarea minRows={2} placeholder='Enter Description...' value={description} onChange={handleDescriptionChange}  sx={{bgcolor:"#A0BFE0", marginTop:"20px"}} />
         </Container>
      
       )

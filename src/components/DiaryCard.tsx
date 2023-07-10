c5dc269d7aa3a2fc8001687082aae437b920c330
udyogi-siphara <udyogisiphara22@gmail.com>
2023-07-10 10:53:32 +0530

    useState for show more btn

--- src/components/DiaryCard.tsx
+++ src/components/DiaryCard.tsx
@@ -1,7 +1,11 @@
 import {Button,Typography,CardContent,CardActions, Card} from '@mui/material';
 import  React  from "react";
+import { useState } from 'react';
 
 const DiaryCard = (props:any) => {
+
+    const [showMore, setShowMore] = useState(false);
+
     return ( 
     <Card sx={{ width: 300, marginTop:5 }}>
         <CardContent>

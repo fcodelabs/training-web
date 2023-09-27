860afc149d3609395669fc5bb1b9402fa04dec2e
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-27 17:33:23 +0530

    create Firestore

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);

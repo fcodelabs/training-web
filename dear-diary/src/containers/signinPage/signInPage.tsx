fc9be7b9662a98042dfe1f5e03df4dfc57e03e87
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-11-30 22:11:12 +0530

    implemented random function

--- dear-diary/src/containers/signinPage/signInPage.tsx
+++ dear-diary/src/containers/signinPage/signInPage.tsx
@@ -1,12 +1,15 @@
 
-
+import getrandomName from "./Random/randomNameGenerator";
 import "./signInPage.css";
 import { Button } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import logo from "./logo.png"
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useState } from "react";
 
 const SignInPage = () => {
+    const [nameRandom, SetRandomName] = useState("")
+
     return (
         <div className="signin-page-wrapper">
             <div className="signin-page">
@@ -24,9 +27,16 @@ const SignInPage = () => {
                     </div>
 
                     <form className="sign-in-form">
-                        <TextField id="outlined-basic" label="name" variant="outlined" className="text-field-login" size="small" />
+                        <TextField
+                            id="outlined-basic"
+                            label="name"
+                            variant="outlined"
+                            className="text-field-login"
+                            size="small"
+                            value={nameRandom} 
+                            onChange={(e) => SetRandomName(e.target.value)}/>
                         <div className="random-button">
-                            <Button variant="contained" className="btn-random" style={{ textTransform: 'none' }}>
+                            <Button variant="contained" className="btn-random" style={{ textTransform: 'none' }} onClick={() => SetRandomName(getrandomName)}>
                                 Random
                             </Button>
                         </div>
@@ -35,7 +45,7 @@ const SignInPage = () => {
                     <div className="signin-submit-button">
                         <Button variant="contained" className="btn-continue" style={{ textTransform: 'none' }}>
                             Continue
-                            <ArrowForwardIcon/>
+                            <ArrowForwardIcon />
                         </Button>
                     </div>
                 </div>

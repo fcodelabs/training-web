b5a08e4aa196fd2fda83c81d08b21167442a5668
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-01 01:57:55 +0530

    update files

--- dear-diary/src/containers/signinPage/Random/randomNameGenerator.tsx
+++
@@ -1,8 +0,0 @@
-const names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hank', 'Ivy', 'Jack'];
-
-function getRandomName() {
-  const randomIndex = Math.floor(Math.random() * names.length);
-  return names[randomIndex];
-}
-
-export default getRandomName;

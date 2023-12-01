8dcb26ff066c340a2a45baf7aa5a0332b2015ca0
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-01 19:08:28 +0530

    added seacrhbar and modified navbar

--- dear-diary/src/containers/HomePage/Page/HomePage.tsx
+++ dear-diary/src/containers/HomePage/Page/HomePage.tsx
@@ -1,13 +1,13 @@
-import Deardiary from "../../components/DearDiary/DearDiary"
-import Profile from "../../components/Profile/Profile";
-import Home from "./Home";
+import Deardiary from "../../../components/DearDiary/DearDiary"
+import ResponsiveAppBar from "../../../components/Navbar/Navbar";
+import Profile from "../../../components/Profile/Profile";
+import Home from "../Homebody/Home";
 import "./HomePage.css"
 const HomePage = () => {
     return (
         <div className="homepage-wrapper">
             <div className="header-home">
-                <Deardiary />
-                <Profile />
+                <ResponsiveAppBar/>
             </div>
             <div className="home">
                 <Home />

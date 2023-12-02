91b1a832d03ac38f752718923a6646dc1247466c
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-02 11:35:02 +0530

    created diary card

--- dear-diary/src/containers/HomePage/Homebody/Home.tsx
+++ dear-diary/src/containers/HomePage/Homebody/Home.tsx
@@ -2,6 +2,14 @@ import PrimarySearchAppBar from "../../../components/SearchBar/SearchBar";
 import Button from "@mui/material/Button";
 import "./Home.css";
 import SubmitForm from "../SubmitForm/SubmitForm";
+import DiaryCard from "../../../components/DiaryCard/DiaryCard";
+
+const DiaryCardList = [
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's conten,and make up the bulk of the card's contentt,and make up the bulk of the card's content,and make up the bulk of the card's content,and make up the bulk of the card's content" },
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    
+]
 
 interface HomeProps {
     showForm: boolean;
@@ -9,15 +17,23 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({ showForm, reset }) => (
+
     <div className="home-wrapper">
-        <div className="home-header">Home</div>
+        <div className="home-header">Home
+        </div>
         <div className="home-search">
             <PrimarySearchAppBar />
             <Button className="search-button" onClick={reset}>
                 Submit New
             </Button>
         </div>
-        <div className="home-body"></div>
+        <div className="home-body" style={{ display: "flex", flex: "flex-wrap", justifyContent: "flex-start" }}>
+
+            {DiaryCardList.map((diary, key) => (
+                <DiaryCard key={key} diary={diary} />
+            ))}
+
+        </div>
         {(<SubmitForm showForm={showForm} reset={reset} />
         )}
     </div>

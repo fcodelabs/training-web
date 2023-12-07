2aaeb4cde02950bd1839856a15c7ade2b08b1dd7
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-07 13:02:04 +0530

    implemented search function

--- dear-diary/src/containers/HomePage/Homebody/Home.tsx
+++ dear-diary/src/containers/HomePage/Homebody/Home.tsx
@@ -1,38 +1,42 @@
-import PrimarySearchAppBar from "../../../components/SearchBar/SearchBar";
-import Button from "@mui/material/Button";
-import "./Home.css";
-import SubmitForm from "../SubmitForm/SubmitForm";
-import DiaryCard from "../../../components/DiaryCard/DiaryCard";
-import { useAppSelector } from "../../../redux/store/hooks";
-import { Box } from "@mui/material";
+import React, { useState } from 'react';
+import PrimarySearchAppBar from '../../../components/SearchBar/SearchBar';
+import Button from '@mui/material/Button';
+import './Home.css';
+import SubmitForm from '../SubmitForm/SubmitForm';
+import DiaryCard from '../../../components/DiaryCard/DiaryCard';
+import { useAppSelector } from '../../../redux/store/hooks';
+import { Box, Typography } from '@mui/material';
 
-
-type HomeProps ={
-    showForm: boolean;
-    reset: () => void;
-}
-
+type HomeProps = {
+  showForm: boolean;
+  reset: () => void;
+};
 
 const Home: React.FC<HomeProps> = ({ showForm, reset }) => {
-    const diaries = useAppSelector((state) => state.diaryCard.cards);
-    
-    return (
-        <div className="home-wrapper">
-            <div className="home-header">Home</div>
-            <div className="home-search">
-                <PrimarySearchAppBar />
-                <Button className="search-button" onClick={reset}>
-                    Submit New
-                </Button>
-            </div>
-            <Box className="home-body">
-                {diaries.map((diary, key) => (
-                    <DiaryCard key={key} diary={diary}/>
-                ))}
-            </Box>
-            <SubmitForm showForm={showForm} reset={reset} />
-        </div>
-    );
+  const diaries = useAppSelector((state) => state.diaryCard.cards);
+  const [filteredList, setFilteredList] = useState(diaries);
+
+  return (
+    <div className="home-wrapper">
+      <div className="home-header">Home</div>
+      <div className="home-search">
+        <PrimarySearchAppBar setFilteredList={setFilteredList} />
+        <Button className="search-button" onClick={reset}>
+          Submit New
+        </Button>
+      </div>
+      <Box className="home-body">
+        {filteredList.length === 0 ? (
+          <Typography >No diaries found.</Typography>
+        ) : (
+          filteredList.map((diary, key) => (
+            <DiaryCard key={key} diary={diary} />
+          ))
+        )}
+      </Box>
+      <SubmitForm showForm={showForm} reset={reset} />
+    </div>
+  );
 };
 
 export default Home;
--- dear-diary/src/redux/features/diarycard/diaryCardSlice.ts
+++ dear-diary/src/redux/features/diarycard/diaryCardSlice.ts
@@ -11,15 +11,20 @@ type initialStateType={
 }
 const initialState: initialStateType={
     cards: [
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 1", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 2", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 3", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 4", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 5", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 6", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 7", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title 8", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
   ]
+}
+
+export const searchCards = (searchText: string, cards: Card[]) => {
+    return cards.filter(card=> card.title.toLowerCase().includes(searchText.toLowerCase()));
+
 }
 
 const diaryCardSlice = createSlice({
@@ -29,6 +34,7 @@ const diaryCardSlice = createSlice({
         addCard: (state, action: PayloadAction<Card>) => {
             state.cards.push(action.payload);
         },
+
     },
 });
 

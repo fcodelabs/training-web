c7c52905338a168c26248cef879f6c6cf5002b56
Thambara Sahassaka <114559191+Thambara-20@users.noreply.github.com>
2023-12-07 11:54:15 +0530

    updated signup

--- dear-diary/src/redux/features/diarycard/diaryCardSlice.ts
+++ dear-diary/src/redux/features/diarycard/diaryCardSlice.ts
@@ -14,9 +14,11 @@ const initialState: initialStateType={
     { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
     { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
     { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
     { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
     { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
-    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content" },
+    { title: "Card Title", body: "Some quick example text to build on the card title and make up the bulk of the card's content, Some quick example text to build on the card title and make up the bulk of the card's content" },
   ]
 }
 

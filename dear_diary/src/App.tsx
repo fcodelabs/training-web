20d81e1ad3eeff342f7d392dd278a2085c343100
Wathmiv <wathmi.v@gmail.com>
2023-12-08 13:31:56 +0530

    Created adding form

--- dear_diary/src/App.tsx
+++ dear_diary/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import DiaryHomePage from './Pages/DiaryHome/DiaryHomePage';
 import SignInPage from './Pages/SignInPage/SignInPage';
+import LayoutBackground from './Components/LayoutBackground/LayoutBackground';
+import FormToAddNewDiary from './Components/FormToAddNewDiary/FormToAddNewDiary';
 
 
 function App() {
   return (
-        <DiaryHomePage />
+    <LayoutBackground>
+    <DiaryHomePage/>
+</LayoutBackground>
   );
 }
 

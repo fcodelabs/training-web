bf88ab8aa81ea61ef784c00d07aa025614888d06
Pasindu Madusha <pasindumadusha.20@cse.mrt.ac.lk>
2023-12-09 00:59:12 +0530

    updated the styling with styled components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,41 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import "@fontsource/public-sans/";
+import styled from 'styled-components';
+
+const StyledBox = styled(Box)`
+  bgcolor: rgba(0, 123, 255, 0);
+  height: auto;
+  width: auto;
+  padding: 24px;
+  gap: 12px;
+  align-items: center;
+  display: flex;
+`;
+
+const StyledSpan = styled.span`
+  color: #4b465c;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 24px;
+  font-family: public sans;
+  font-feature-settings: 'clig' off, 'liga' off;
+  height: auto;
+`;
+
+const StyledImg = styled.img`
+  width: 40px;
+  height: 40px;
+`;  
 
 const Header = () => {
     return (
         <div>
-        <Container style={{}}  >
-        <Box sx={{ bgcolor: 'rgba(0, 123, 255, 0)', height: 'auto', width: 'auto', padding: '24px', gap: '12px', alignItems: 'center', display: 'flex', }}>
-        <img src={process.env.PUBLIC_URL+'/image1.png'} alt="Logo" style={{ width: '40px', height: '40px',}} />
-          <span style={{ color: "#4B465C", fontSize: "24px", fontWeight: '500', lineHeight: '24px', fontFamily: 'public sans', fontFeatureSettings: "'clig' off, 'liga' off", height: 'auto' }}>Dear Diary</span>
-        </Box>
+        <Container>
+        <StyledBox>
+        <StyledImg src={process.env.PUBLIC_URL+'/image1.png'} alt="Logo"/>
+          <StyledSpan>Dear Diary</StyledSpan>
+        </StyledBox>
         
       </Container>
         </div>
--- src/containers/HomePage/SubmitCard/SubmitCard.tsx
+++ src/containers/HomePage/SubmitCard/SubmitCard.tsx
@@ -1,154 +1,203 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, IconButton } from '@mui/material';
-import { useState } from 'react';
-import "@fontsource/public-sans/";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import styled from 'styled-components';
 
+const StyledContainer = styled.div`
+  width: 400px;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  position: relative;
+  height: 100vh;
+`;
 
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const StyledTitle = styled.span`
+  color: #4B465C;
+  font-family: public sans;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`;
+
+const StyledCloseButton = styled(IconButton)`
+&&&{
+  background-color: #4B465C29;
+  width: 24px;
+  height: 24px;
+  border-radius: 6px;
+  padding: 7px;
+}
+`;
+
+const StyledSpan = styled.span`
+  margin-bottom: 8px;
+  margin-right: 330px;
+  font-family: public sans;
+  font-feature-settings: 'clig' off, 'liga' off;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #4B465C;
+`;
+
+const StyledTextFieldContainer = styled.div`
+  margin-bottom: 20px;
+`;
+
+const StyledTextFieldInput = styled(TextField)`
+  margin-top: 10px;
+`;
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const StyledSubmitButton = styled(Button)`
+&&&{
+  width: 94px;
+  height: 38px;
+  padding: 10px 20px;
+  gap: 12px;
+  border-radius: 6px;
+  font-family: public sans;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+  color: #ffffff;
+  background-color: #0092DD;
+  letter-spacing: 0.43px;
+  text-transform: none;
+}
+`;
+
+const StyledCancelButton = styled(Button)`
+&&&{
+  width: 94px;
+  height: 38px;
+  padding: 10px 20px;
+  gap: 12px;
+  border-radius: 6px;
+  font-family: public sans;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 18px;
+  color: #A8AAAE;
+  background-color: rgba(168, 170, 174, 0.16);
+  letter-spacing: 0.43px;
+  text-transform: none;
+}
+`;
+
+const StyledToast = styled(ToastContainer)`
+  position: top-right;
+`;
+
+// define interface to represent component props
 interface SubmitCardProps {
-    onClose: () => void;
-    onsubmit: (title: string, description: string) => void;
+  onClose: () => void;
+  onsubmit: (title: string, description: string) => void;
 }
 
 const SubmitCard: React.FC<SubmitCardProps> = ({ onClose, onsubmit }) => {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
+  const [title, setTitle] = useState('');             // add state for title
+  const [description, setDescription] = useState(''); // add state for description
 
-    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value);
-    };
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value); // update title state
+  };
 
-    const handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setDescription(event.target.value);
-    };
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value); // update description state
+  };
 
-    const handleSubmit = () => {
-        console.log('Title:', title);
-        console.log('Description:', description);
-        if (title === '') {
-            toast.error('Please enter title');
-            return;
-        };
-        if (description === '') {
-            toast.error('Please enter description');
-            return;
-        };
-        if (title === '' || description === '') {
-            toast.error('Please enter title and description');
-            return;
-        };
-        if (title !== '' && description !== '') {
-            toast.success('Submitted successfully');
-        };
-        onsubmit(title, description);
-        onClose();
-    };
+  const handleSubmit = () => {
+    // add validation for title and description
+    // if title is empty, show toast error message
+    if (title === '') {
+      toast.error('Please enter title');
+      return;
+    }
+    // if description is empty, show toast error message
+    if (description === '') {
+      toast.error('Please enter description');
+      return;
+    }
+    // if title and description are empty, show toast error message
+    if (title === '' || description === '') {
+      toast.error('Please enter title and description');
+      return;
+    }
+    // if title and description are not empty, show toast success message
+    if (title !== '' && description !== '') {
+      toast.success('Submitted successfully');
+    }
+    // call onsubmit function with title and description
+    onsubmit(title, description);
+    onClose();
+  };
 
-    return (
-        <div style={{ width: '400px', backgroundColor: '#fff', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', boxSizing: 'border-box', position: 'relative', height: '100vh' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
-                <span style={{
-                    color: '#4B465C',
-                    fontFamily: 'public sans',
-                    fontFeatureSettings: "'clig' off, 'liga' off",
-                    fontSize: '18px',
-                    fontStyle: 'normal',
-                    fontWeight: 500,
-                    lineHeight: '24px',
-                }}>Submit New</span>
-                <IconButton style={{ backgroundColor: '#4B465C29', width: '24px', height: '24px', borderRadius: '6px', padding: '7px' }} color="inherit" onClick={onClose}>
-                    <img src={process.env.PUBLIC_URL+'close.svg'} alt="Close Icon" />
-                </IconButton>
-            </div>
-            <form>
-                <div style={{ marginBottom: '20px' }}>
-                    <span style={{
-                        marginBottom: '8px', marginRight: '330px',
-                        fontFamily: 'public sans',
-                        fontFeatureSettings: "'clig' off, 'liga' off",
-                        fontSize: '13px',
-                        fontStyle: 'normal',
-                        fontWeight: 400,
-                        lineHeight: 'normal',
-                        color: '#4B465C'
-                    }}>Title</span>
-                    <TextField
-                        style={{ marginTop: '10px' }}
-                        label="Placeholder"
-                        variant="outlined"
-                        fullWidth
-                        size='small'
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                </div>
-                <div style={{ marginBottom: '20px' }}>
-                    <span style={{
-                        marginBottom: '8px', marginRight: '330px',
-                        fontFamily: 'public sans',
-                        fontFeatureSettings: "'clig' off, 'liga' off",
-                        fontSize: '13px',
-                        fontStyle: 'normal',
-                        fontWeight: 400,
-                        lineHeight: 'normal',
-                        color: '#4B465C'
-                    }}>Description</span>
-                    <TextField
-                        style={{ marginTop: '10px' }}
-                        label="Placeholder"
-                        variant="outlined"
-                        multiline
-                        rows={13}
-                        fullWidth
-                        value={description}
-                        onChange={handleDescriptionChange}
-                    />
-                </div>
-                <div style={{ display: 'flex', gap: '20px' }}>
-                    <Button
-                        style={{
-                            width: '94px', height: '38px', padding: '10px 20px', gap: '12px', borderRadius: '6px',
-                            fontFamily: 'public sans',
-                            fontSize: '15px',
-                            fontStyle: 'normal',
-                            fontWeight: 500,
-                            lineHeight: '18px',
-                            color: '#ffffff',
-                            backgroundColor: '#0092DD',
-                            letterSpacing: '0.43px',
-                            textTransform: 'none',
-                        }}
-                        variant="contained"
-                        disableElevation
-                        onClick={handleSubmit}
-                    >
-                        Submit
-                    </Button>
-                    <Button
-                        style={{
-                            width: '94px', height: '38px', padding: '10px 20px', gap: '12px', borderRadius: '6px',
-                            fontFamily: 'public sans',
-                            fontSize: '15px',
-                            fontStyle: 'normal',
-                            fontWeight: 500,
-                            lineHeight: '18px',
-                            color: '#A8AAAE',
-                            backgroundColor: 'rgba(168, 170, 174, 0.16)',
-                            letterSpacing: '0.43px',
-                            textTransform: 'none',
-                        }}
-                        variant="contained"
-                        disableElevation
-                        onClick={onClose}
-                    >
-                        Cancel
-                    </Button>
-                </div>
-                <ToastContainer position="top-right" autoClose={3000} />
-            </form>
-        </div>
-    );
+  return (
+    <StyledContainer>
+      <StyledHeader>
+        <StyledTitle>Submit New</StyledTitle>
+        <StyledCloseButton color="inherit" onClick={onClose}>
+          <img src={process.env.PUBLIC_URL + 'close.svg'} alt="Close Icon" />
+        </StyledCloseButton>
+      </StyledHeader>
+      <form>
+        <StyledTextFieldContainer>
+          <StyledSpan>Title</StyledSpan>
+          <StyledTextFieldInput
+            label="Placeholder"
+            variant="outlined"
+            fullWidth
+            size="small"
+            value={title}
+            onChange={handleTitleChange} // update title state
+          />
+        </StyledTextFieldContainer>
+        <StyledTextFieldContainer>
+          <StyledSpan>Description</StyledSpan>
+          <StyledTextFieldInput
+            label="Placeholder"
+            variant="outlined"
+            multiline
+            rows={13} // set rows to 13
+            fullWidth
+            value={description}
+            onChange={handleDescriptionChange} // update description state
+          />
+        </StyledTextFieldContainer>
+        <StyledButtonContainer>
+          <StyledSubmitButton variant="contained" disableElevation onClick={handleSubmit}>
+            Submit
+          </StyledSubmitButton>
+          <StyledCancelButton variant="contained" disableElevation onClick={onClose}>
+            Cancel
+          </StyledCancelButton>
+        </StyledButtonContainer>
+        {/* add toast container to show toast messages */}
+        <StyledToast position="top-right" autoClose={3000} />
+      </form>
+    </StyledContainer>
+  );
 };
 
 export default SubmitCard;
